f8b7215171ee6c6c6824d87b3589b8dbf74f122a
JUMAn <nllngtn@gmail.com>
2024-10-30 13:49:09 -0700

    first commit

---
+++ SimpleCalculator.java
@@ -0,0 +1,18 @@
+public class SimpleCalculator{
+
+    public static void multiplenumber(){
+
+        int num1=4;
+        int num2=8;
+
+        int answer=num1*num2;
+
+        System.out.println(answer);
+
+    }
+
+    public static void main (String[] args){
+
+        multiplenumber();
+    }
+}
